c9a24f200c75ecec0c2f3567e8862121faa77b25
TripleS <triple-s-76@outlook.com>
2023-11-19 10:41:48 -0800

    created Node class

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,2 +1,11 @@
 require_relative '../main'
 
+describe Node do
+  let(:my_node) { Node.new(123) }
+  it 'tests the node class' do
+    my_node.data = 123
+    expect(my_node.data).to eq(123)
+    expect(my_node.left).to be_nil
+    expect(my_node.right).to be_nil
+  end
+end
