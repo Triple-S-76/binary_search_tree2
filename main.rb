c3f393a23789efd9af9c164d3d00b60738229d80
TripleS <triple-s-76@outlook.com>
2023-11-20 05:37:15 -0800

    Added #build_tree, #insert, #pretty_print methods and tests

--- main.rb
+++ main.rb
@@ -1,3 +1,6 @@
+require 'pry-byebug'
+
+# Creates each individual node
 class Node
   attr_accessor :data, :left, :right
 
@@ -8,10 +11,84 @@ class Node
   end
 end
 
+# Builds the binary search tree
 class Tree
-  attr_accessor :array
+  attr_accessor :root
 
-  def initialize(array)
-    @array = array
+  def initialize
+    @root = nil
+  end
+
+  def build_tree(array)
+    sorted_array = array.sort.uniq
+
+    @root = build_tree_recursive(sorted_array)
+  end
+
+  def insert(data)
+    @root = Node.new(data) if @root.nil?
+
+    insert_recursive(data, @root)
+  end
+
+  def pretty_print
+    pretty_print_private
+  end
+
+  private
+
+  def build_tree_recursive(array, start_index = 0, end_index = array.length - 1)
+    return nil if start_index > end_index
+
+    mid = (start_index + end_index) / 2
+
+    new_node = Node.new(array[mid])
+    new_node.left = build_tree_recursive(array, start_index, mid - 1)
+    new_node.right = build_tree_recursive(array, mid + 1, end_index)
+
+    new_node
+  end
+
+  def insert_recursive(data, tree)
+    if data < tree.data
+      return insert_data(data, tree, 'left') if tree.left.nil?
+
+      insert_recursive(data, tree.left)
+    elsif data > tree.data
+      return insert_data(data, tree, 'right') if tree.right.nil?
+
+      insert_recursive(data, tree.right)
+    else
+      puts 'This data is already in the tree'
+    end
+  end
+
+  def insert_data(data, tree, side)
+    new_node = Node.new(data)
+
+    if side == 'left'
+      tree.left = new_node
+    else
+      tree.right = new_node
+    end
+  end
+
+  def pretty_print_private(node = @root, prefix = '', is_left = true)
+    pretty_print_private(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print_private(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 end
+
+my_array = [5, 6, 6, 7, 4, 5, 5, 4, 5, 6, 6, 1, 2, 3, 4, 5, 6, 7]
+my_tree = Tree.new
+my_tree.build_tree(my_array)
+3.times { puts }
+my_tree.pretty_print
+3.times { puts }
+my_tree.insert(99)
+my_tree.insert(-99)
+my_tree.insert(5.5)
+my_tree.pretty_print
+3.times { puts }
+
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../main'
+require 'pry-byebug'
 
 describe Node do
   let(:my_node) { Node.new(123) }
@@ -10,9 +11,44 @@ describe Node do
 end
 
 describe Tree do
-  let(:my_tree) { Tree.new([1, 2, 3]) }
+  context 'Basic tests' do
+    before(:each) { @my_tree = Tree.new }
 
-  it 'tests initialize in Tree class' do
-    expect(my_tree.array).to eq([1, 2, 3])
+    it 'tests initialize in Tree class' do
+      expect(@my_tree.root).to be_nil
+    end
+
+    it 'tests the #build_tree method' do
+      array = [5, 3, 3, 5, 1, 1]
+      @my_tree.build_tree(array)
+
+      expect(@my_tree.root.data).to eq(3)
+      expect(@my_tree.root.left.data).to eq(1)
+      expect(@my_tree.root.right.data).to eq(5)
+    end
+  end
+
+  context 'Inserting nodes' do
+
+    before(:each) do
+      @my_tree = Tree.new
+      array = [20, 30, 40, 50, 60, 70, 80]
+      @my_tree.build_tree(array)
+    end
+
+    it 'inserts a node higher than all nodes' do
+      @my_tree.insert(90)
+      expect(@my_tree.root.right.right.right.data).to eq(90)
+    end
+
+    it 'inserts a node lower than all nodes' do
+      @my_tree.insert(10)
+      expect(@my_tree.root.left.left.left.data).to eq(10)
+    end
+
+    it 'inserts a node in the middle of the tree' do
+      @my_tree.insert(65)
+      expect(@my_tree.root.right.left.right.data).to eq(65)
+    end
   end
 end
