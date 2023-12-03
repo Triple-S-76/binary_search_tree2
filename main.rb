afd9d854d9c365dae21b732eb5bc9be3dae3773a
TripleS <triple-s-76@outlook.com>
2023-12-03 02:28:30 -0800

    Finished project and tests

--- main.rb
+++ main.rb
@@ -31,6 +31,233 @@ class Tree
     insert_recursive(data, @root)
   end
 
+  def delete(data)
+    if @root.nil?
+      'The tree is empty'
+    elsif @root.data == data
+      delete_root_node
+    else
+      delete_node(data)
+    end
+  end
+
+  def delete_root_node
+    root_children = node_children_info(nil)
+    case root_children
+    when 'leaf'
+      @root = nil
+    when 'left child'
+      @root = @root.left
+    when 'right child'
+      @root = @root.right
+    else
+      node_to_move = find_next_largest_node(@root)
+      node_to_move_parent = find_parent_node(node_to_move.data)
+      node_to_move_parent.left = node_to_move.right unless node_to_move == @root.right
+      node_to_move.left = @root.left
+      node_to_move.right = @root.right unless node_to_move == @root.right
+      @root = node_to_move
+    end
+  end
+
+  def delete_node(data)
+    parent_node = find_parent_node(data)
+    return parent_node if parent_node.instance_of?(String)
+
+    node_children = node_children_info(data, parent_node)
+    remove_node(parent_node, node_children, data)
+  end
+
+  def find_parent_node(data, subtree = @root, parent_node = nil)
+    return parent_node if data == subtree.data
+
+    if data < subtree.data
+      return 'This is not in the tree' if subtree.left.nil?
+
+      find_parent_node(data, subtree.left, subtree)
+    else
+      return 'This is not in the tree' if subtree.right.nil?
+
+      find_parent_node(data, subtree.right, subtree)
+    end
+  end
+
+  def node_children_info(data, parent_node = nil)
+    return get_children_info(@root) if parent_node.nil?
+
+    if data < parent_node.data
+      get_children_info(parent_node.left)
+    else
+      get_children_info(parent_node.right)
+    end
+  end
+
+  def get_children_info(node)
+    if node.left.nil? && node.right.nil?
+      'leaf'
+    elsif !node.left.nil? && node.right.nil?
+      'left child'
+    elsif node.left.nil? && !node.right.nil?
+      'right child'
+    else
+      '2 children'
+    end
+  end
+
+  def remove_node(parent_node, node_children, data = nil)
+    case node_children
+    when 'leaf'
+      remove_leaf_node(parent_node, data)
+    when 'left child'
+      remove_node_with_left_child(parent_node, data)
+    when 'right child'
+      remove_node_with_right_child(parent_node, data)
+    else
+      remove_node_with_2_children(parent_node, data)
+    end
+  end
+
+  def remove_leaf_node(parent_node, data)
+    if data < parent_node.data
+      parent_node.left = nil
+    else
+      parent_node.right = nil
+    end
+  end
+
+  def remove_node_with_left_child(parent_node, data)
+    if data < parent_node.data
+      parent_node.left = parent_node.left.left
+    else
+      parent_node.right = parent_node.right.left
+    end
+  end
+
+  def remove_node_with_right_child(parent_node, data)
+    if data < parent_node.data
+      parent_node.left = parent_node.left.right
+    else
+      parent_node.right = parent_node.right.right
+    end
+  end
+
+  def remove_node_with_2_children(parent_node, data)
+    node_to_delete = data < parent_node.data ? parent_node.left : parent_node.right
+
+    node_to_move = find_next_largest_node(node_to_delete)
+    node_to_move_parent = find_parent_node(data, node_to_delete, node_to_delete)
+    node_to_move_parent.left = node_to_move.right
+    node_to_move.left = node_to_delete.left
+    node_to_move.right = node_to_delete.right unless node_to_move == node_to_delete.right
+    data < parent_node.data ? parent_node.left = node_to_move : parent_node.right = node_to_move
+  end
+
+  def find(data, node = @root)
+    return node if data == node.data
+
+    data < node.data ? find(data, node.left) : find(data, node.right)
+  end
+
+  def find_next_largest_node(node_to_delete)
+    node_to_move = node_to_delete.right
+    until node_to_move.left.nil?
+      node_to_move = node_to_move.left
+    end
+    node_to_move
+  end
+
+  def level_order
+    array = []
+    queue = [@root]
+    until queue.empty?
+      current_node = queue.shift
+      array << current_node.data
+      queue << current_node.left unless current_node.left.nil?
+      queue << current_node.right unless current_node.right.nil?
+    end
+    array
+  end
+
+  def level_order_recursive(array = [], queue = [@root])
+    return array if queue.empty?
+
+    current_node = queue.shift
+    array << current_node.data
+    queue << current_node.left unless current_node.left.nil?
+    queue << current_node.right unless current_node.right.nil?
+    level_order_recursive(array, queue)
+  end
+
+  def inorder(current_node = root, array = [])
+    return if current_node.nil?
+
+    inorder(current_node.left, array)
+    array << current_node.data
+    inorder(current_node.right, array)
+    array
+  end
+
+  def preorder(current_node = root, array = [])
+    return if current_node.nil?
+
+    array << current_node.data
+    preorder(current_node.left, array)
+    preorder(current_node.right, array)
+    array
+  end
+
+  def postorder(current_node = root, array = [])
+    return if current_node.nil?
+
+    postorder(current_node.left, array)
+    postorder(current_node.right, array)
+    array << current_node.data
+    array
+  end
+
+  def height(current_node = root, current_height = -1, max_height = 0)
+    return max_height if current_node.nil?
+
+    current_height += 1
+
+    max_height = current_height if current_height > max_height
+    left_height = height(current_node.left, current_height, max_height)
+    right_height = height(current_node.right, current_height, max_height)
+    [left_height, right_height, max_height].max
+  end
+
+  def depth(data)
+    current_node = @root
+    depth = 0
+    until current_node.nil? || current_node.data == data
+      depth += 1
+      current_node = if data < current_node.data
+                       current_node.left
+                     else
+                       current_node.right
+                     end
+    end
+    current_node.nil? ? -1 : depth
+  end
+
+  def balanced?(current_node = @root, current_height = -1, array = [])
+    return if current_node.nil?
+
+    current_height += 1
+    if current_node.left.nil? && current_node.right.nil?
+      array << current_height
+    else
+      balanced?(current_node.left, current_height, array)
+      balanced?(current_node.right, current_height, array)
+    end
+    array.max - array.min <= 1
+  end
+
+  def rebalance
+    new_array = inorder
+    build_tree(new_array)
+  end
+
   def pretty_print
     pretty_print_private
   end
@@ -80,15 +307,33 @@ class Tree
   end
 end
 
-my_array = [5, 6, 6, 7, 4, 5, 5, 4, 5, 6, 6, 1, 2, 3, 4, 5, 6, 7]
-my_tree = Tree.new
-my_tree.build_tree(my_array)
-3.times { puts }
-my_tree.pretty_print
-3.times { puts }
-my_tree.insert(99)
-my_tree.insert(-99)
-my_tree.insert(5.5)
-my_tree.pretty_print
-3.times { puts }
+starting_array = []
+25.times do
+  starting_array << rand(1..100)
+end
+p starting_array
 
+my_tree = Tree.new
+my_tree.build_tree(starting_array)
+p my_tree.balanced?
+
+p "Level order: #{my_tree.level_order}"
+p "Level order recursive: #{my_tree.level_order_recursive}"
+p "Preorder: #{my_tree.preorder}"
+p "Inorder: #{my_tree.inorder}"
+p "Postorder: #{my_tree.postorder}"
+
+10.times do
+  random_number_over_1000 = rand(1000..10000)
+  my_tree.insert(random_number_over_1000)
+end
+
+p my_tree.balanced?
+my_tree.rebalance
+p my_tree.balanced?
+
+p "Level order: #{my_tree.level_order}"
+p "Level order recursive: #{my_tree.level_order_recursive}"
+p "Preorder: #{my_tree.preorder}"
+p "Inorder: #{my_tree.inorder}"
+p "Postorder: #{my_tree.postorder}"
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -25,11 +25,14 @@ describe Tree do
       expect(@my_tree.root.data).to eq(3)
       expect(@my_tree.root.left.data).to eq(1)
       expect(@my_tree.root.right.data).to eq(5)
+      expect(@my_tree.root.left.left).to be_nil
+      expect(@my_tree.root.left.right).to be_nil
+      expect(@my_tree.root.right.left).to be_nil
+      expect(@my_tree.root.right.right).to be_nil
     end
   end
 
   context 'Inserting nodes' do
-
     before(:each) do
       @my_tree = Tree.new
       array = [20, 30, 40, 50, 60, 70, 80]
@@ -49,6 +52,486 @@ describe Tree do
     it 'inserts a node in the middle of the tree' do
       @my_tree.insert(65)
       expect(@my_tree.root.right.left.right.data).to eq(65)
+    end
+  end
+
+  context 'Deleting Nodes' do
+    before(:each) do
+      @my_tree = Tree.new
+      array = [20, 30, 40, 50, 60, 70, 80]
+      @my_tree.build_tree(array)
+    end
+
+    it '#delete - Attempts to delete a node in an empty tree' do
+      @my_empty_tree = Tree.new
+      error = @my_empty_tree.delete(123)
+      expect(error).to eq('The tree is empty')
+    end
+
+    it '#delete - sends message to delete root node' do
+      my_root_node_tree = Tree.new
+      array = [10, 20, 30]
+      my_root_node_tree.build_tree(array)
+      expect(my_root_node_tree.root.data).to eq(20)
+      expect(my_root_node_tree).to receive(:delete_root_node).once
+      my_root_node_tree.delete(20)
+    end
+
+    it '#delete_root_node - deletes the root node with no children' do
+      my_tree_with_only_root = Tree.new
+      my_tree_with_only_root.build_tree([99])
+      expect(my_tree_with_only_root.root.data).to eq(99)
+      expect(my_tree_with_only_root.root.left).to be_nil
+      expect(my_tree_with_only_root.root.right).to be_nil
+      my_tree_with_only_root.delete(99)
+      expect(my_tree_with_only_root.root).to be_nil
+    end
+
+    it '#delete_root_node - with left child only' do
+      my_tree = Tree.new
+      my_tree.build_tree([20])
+      my_tree.insert(10)
+      expect(my_tree.root.data).to eq(20)
+      expect(my_tree.root.left.data).to eq(10)
+      my_tree.delete(20)
+      expect(my_tree.root.data).to eq(10)
+    end
+
+    it '#delete_root_node - with right child only' do
+      my_tree = Tree.new
+      my_tree.build_tree([10, 20])
+      expect(my_tree.root.data).to eq(10)
+      expect(my_tree.root.right.data).to eq(20)
+      my_tree.delete(10)
+      expect(my_tree.root.data).to eq(20)
+    end
+
+    it '#delete_root_node - with 2 children - right child is leaf node' do
+      my_tree = Tree.new
+      my_tree.build_tree([10, 20, 30])
+      expect(my_tree.root.data).to eq(20)
+      expect(my_tree.root.left.data).to eq(10)
+      expect(my_tree.root.right.data).to eq(30)
+      my_tree.delete_root_node
+      expect(my_tree.root.data).to eq(30)
+      expect(my_tree.root.left.data).to eq(10)
+      expect(my_tree.root.right).to be_nil
+    end
+
+    it '#delete_root_node - with 2 children - both sides have many children' do
+      my_tree = Tree.new
+      array = [0]
+      50.times { array << array[-1] + 1 }
+      my_tree.build_tree(array)
+      expect(my_tree.root.data).to eq(25)
+      expect(my_tree.root.left.data).to eq(12)
+      expect(my_tree.root.right.data).to eq(38)
+      my_tree.delete_root_node
+      expect(my_tree.root.data).to eq(26)
+      expect(my_tree.root.left.data).to eq(12)
+      expect(my_tree.root.right.data).to eq(38)
+      expect(my_tree.root.right.left.left.left.data).to eq(27)
+    end
+
+    it '#delete_root_node - with 2 children - both sides have many children' do
+      my_tree = Tree.new
+      array = [0]
+      200.times { array << array[-1] + 1 }
+      my_tree.build_tree(array)
+      expect(my_tree.root.data).to eq(100)
+      expect(my_tree.root.left.data).to eq(49)
+      expect(my_tree.root.right.data).to eq(150)
+      my_tree.delete_root_node
+      expect(my_tree.root.data).to eq(101)
+      expect(my_tree.root.left.data).to eq(49)
+      expect(my_tree.root.right.data).to eq(150)
+      expect(my_tree.root.right.left.left.left.left.left.data).to eq(102)
+    end
+
+    it 'Deletes node with no children' do
+      expect(@my_tree).to receive(:delete_node).with(60).once
+      @my_tree.delete(60)
+    end
+
+    it '#node_children_info - sends left when data is lower' do
+      test_node = @my_tree.root.right
+      expect(@my_tree).to receive(:get_children_info).with(test_node.left).once
+      @my_tree.node_children_info(60, test_node)
+    end
+
+    it '#node_children_info - sends left when data is higher' do
+      test_node = @my_tree.root.right
+      expect(@my_tree).to receive(:get_children_info).with(test_node.right).once
+      @my_tree.node_children_info(80, test_node)
+    end
+
+    it '#get_children_info - returns "leaf" when node is leaf node' do
+      node_instance_double = instance_double(Node)
+      allow(node_instance_double).to receive_message_chain(:left, :nil?).and_return(true)
+      allow(node_instance_double).to receive_message_chain(:right, :nil?).and_return(true)
+      child_info = @my_tree.get_children_info(node_instance_double)
+      expect(child_info).to eq('leaf')
+    end
+
+    it '#get_children_info - returns "left child" when node has a left child' do
+      node_instance_double = instance_double(Node)
+      allow(node_instance_double).to receive(:left).and_return(false, false)
+      allow(node_instance_double).to receive_message_chain(:right, :nil?).and_return(true)
+      child_info = @my_tree.get_children_info(node_instance_double)
+      expect(child_info).to eq('left child')
+    end
+
+    it '#get_children_info - returns "right child" when node has a right child' do
+      node_instance_double = instance_double(Node)
+      allow(node_instance_double).to receive_message_chain(:left, :nil?).and_return(true)
+      allow(node_instance_double).to receive_message_chain(:right, :nil?).and_return(false)
+      child_info = @my_tree.get_children_info(node_instance_double)
+      expect(child_info).to eq('right child')
+    end
+
+    it '#get_children_info - returns "2 children" when node has 2 children' do
+      node_instance_double = instance_double(Node)
+      allow(node_instance_double).to receive_message_chain(:left, :nil?).and_return(false)
+      allow(node_instance_double).to receive_message_chain(:right, :nil?).and_return(false)
+      child_info = @my_tree.get_children_info(node_instance_double)
+      expect(child_info).to eq('2 children')
+    end
+
+    it '#delete_node - make sure remove_node is sending correct data' do
+      parent_node_double = double(Node)
+      allow(@my_tree).to receive(:find_parent_node).and_return(parent_node_double)
+      allow(@my_tree).to receive(:node_children_info).and_return('leaf')
+      expect(@my_tree).to receive(:remove_node).with(parent_node_double, 'leaf', 123).once
+      @my_tree.delete_node(123)
+    end
+
+    context '#remove_node - correct message sent in each case' do
+      before(:each) { @parent_node_double = double(Node) }
+
+      it '#remove_node - send message: remove_leaf_node(parent_node)' do
+        expect(subject).to receive(:remove_leaf_node)
+        subject.remove_node(@parent_node_double, 'leaf')
+      end
+
+      it '#remove_node - send message: remove_node_with_left_child(parent_node)' do
+        expect(subject).to receive(:remove_node_with_left_child)
+        subject.remove_node(@parent_node_double, 'left child')
+      end
+
+      it '#remove_node - send message: remove_node_with_right_child(parent_node)' do
+        expect(subject).to receive(:remove_node_with_right_child)
+        subject.remove_node(@parent_node_double, 'right child')
+      end
+
+      it '#remove_node - send message: remove_node_with_2_children(parent_node)' do
+        expect(subject).to receive(:remove_node_with_2_children)
+        subject.remove_node(@parent_node_double, '2 children')
+      end
+    end
+
+    context 'removing different nodes with different children' do
+      before(:each) do
+        @my_tree = Tree.new
+        array = [20, 30, 40, 50, 60, 70, 80]
+        @my_tree.build_tree(array)
+        @my_tree.insert(15)
+        @my_tree.insert(45)
+        @my_tree.insert(65)
+        @my_tree.insert(75)
+      end
+
+      it '#remove_leaf_node - left side' do
+        parent_node = @my_tree.root.left.left
+        expect(parent_node.data).to eq(20)
+        node_to_delete = parent_node.left
+        expect(node_to_delete.data).to eq(15)
+        data = node_to_delete.data
+        expect(data).to eq(15)
+        @my_tree.remove_leaf_node(parent_node, data)
+        expect(parent_node.left).to be_nil
+        expect(parent_node.right).to be_nil
+      end
+
+      it '#remove_leaf_node - right side' do
+        parent_node = @my_tree.root.left.right
+        expect(parent_node.data).to eq(40)
+        node_to_delete = parent_node.right
+        expect(node_to_delete.data).to eq(45)
+        data = node_to_delete.data
+        expect(data).to eq(45)
+        @my_tree.remove_leaf_node(parent_node, data)
+        expect(parent_node.left).to be_nil
+        expect(parent_node.right).to be_nil
+      end
+
+      it '#remove_node_with_left_child - node to delete is left of parent' do
+        parent_node = @my_tree.root.left
+        expect(parent_node.data).to eq(30)
+        node_to_delete = parent_node.left
+        expect(node_to_delete.data).to eq(20)
+        expect(node_to_delete.left.data).to eq(15)
+        expect(node_to_delete.right).to be_nil
+        @my_tree.remove_node_with_left_child(parent_node, 20)
+        expect(parent_node.left.data).to eq(15)
+      end
+
+      it '#remove_node_with_left_child - node to delete is right of parent' do
+        parent_node = @my_tree.root.right
+        expect(parent_node.data).to eq(70)
+        node_to_delete = parent_node.right
+        expect(node_to_delete.data).to eq(80)
+        expect(node_to_delete.left.data).to eq(75)
+        expect(node_to_delete.right).to be_nil
+        @my_tree.remove_node_with_left_child(parent_node, 80)
+        expect(parent_node.right.data).to eq(75)
+      end
+
+      it '#remove_node_with_right_child - node to delete is left of parent' do
+        parent_node = @my_tree.root.right
+        expect(parent_node.data).to eq(70)
+        node_to_delete = parent_node.left
+        expect(node_to_delete.data).to eq(60)
+        expect(node_to_delete.left).to be_nil
+        expect(node_to_delete.right.data).to eq(65)
+        @my_tree.remove_node_with_right_child(parent_node, 60)
+        expect(parent_node.left.data).to eq(65)
+        expect(parent_node.left.right).to be_nil
+        expect(parent_node.left.left).to be_nil
+      end
+
+      it '#remove_node_with_right_child - node to delete is right of parent' do
+        parent_node = @my_tree.root.left
+        expect(parent_node.data).to eq(30)
+        node_to_delete = parent_node.right
+        expect(node_to_delete.data).to eq(40)
+        expect(node_to_delete.left).to be_nil
+        expect(node_to_delete.right.data).to eq(45)
+        @my_tree.remove_node_with_right_child(parent_node, 40)
+        expect(parent_node.right.data).to eq(45)
+        expect(parent_node.right.left).to be_nil
+        expect(parent_node.right.right).to be_nil
+      end
+    end
+
+    context 'removing nodes with 2 children' do
+      before(:each) do
+        @my_tree = Tree.new
+        array = [20, 30, 40, 50, 60, 70, 80]
+        @my_tree.build_tree(array)
+        @my_tree.insert(15)
+        @my_tree.insert(45)
+        @my_tree.insert(65)
+        @my_tree.insert(75)
+        @my_tree.insert(35)
+        @my_tree.insert(34)
+        @my_tree.insert(36)
+        @my_tree.insert(43)
+        @my_tree.insert(44)
+        @my_tree.insert(42)
+        @my_tree.insert(42.5)
+        @my_tree.insert(41)
+      end
+
+      it '#remove_node_with_2_children - node to move has no children' do
+        parent_node = @my_tree.root.left
+        expect(parent_node.data).to eq(30)
+        node_to_delete = parent_node.right
+        expect(node_to_delete.data).to eq(40)
+        expect(node_to_delete.left.data).to eq(35)
+        expect(node_to_delete.right.data).to eq(45)
+        @my_tree.remove_node_with_2_children(parent_node, 40)
+        expect(parent_node.right.data).to eq(41)
+      end
+
+      it '#remove_node_with_2_children - node to move has a right child' do
+        @my_tree.insert(41.5)
+        parent_node = @my_tree.root.left
+        expect(parent_node.data).to eq(30)
+        node_to_delete = parent_node.right
+        expect(node_to_delete.data).to eq(40)
+        expect(node_to_delete.left.data).to eq(35)
+        expect(node_to_delete.right.data).to eq(45)
+        @my_tree.remove_node_with_2_children(parent_node, 40)
+        expect(parent_node.right.data).to eq(41)
+      end
+
+      it '#remove_node_with_2_children - node to move is node to removes right child' do
+        parent_node = @my_tree.root.left.right.right.left
+        expect(parent_node.data).to eq(43)
+        node_to_delete = parent_node.left
+        expect(node_to_delete.data).to eq(42)
+        expect(node_to_delete.left.data).to eq(41)
+        expect(node_to_delete.right.data).to eq(42.5)
+        @my_tree.remove_node_with_2_children(parent_node, 42)
+      end
+    end
+
+  end
+  context 'misc tests' do
+    it '#find_parent_node - finds parent node' do
+      my_tree = Tree.new
+      array = [20, 30, 40, 50, 60, 70, 80]
+      my_tree.build_tree(array)
+      parent_node30 = my_tree.find_parent_node(20)
+      expect(parent_node30.data).to eq(30)
+
+      parent_node50 = my_tree.find_parent_node(70)
+      expect(parent_node50.data).to eq(50)
+    end
+
+    it '#find - returns node when node with data exists' do
+      my_tree = Tree.new
+      array = [0]
+      100.times { array << array[-1] + 1 }
+      my_tree.build_tree(array)
+      node = my_tree.find(33)
+      expect(node.data).to eq(33)
+    end
+
+    it '#height - returns an integer of the number of edges to the furthest leaf node' do
+      my_tree = Tree.new
+      array = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+      my_tree.build_tree(array)
+      height = my_tree.height
+      expect(height).to eq(3)
+    end
+
+    context '#depth' do
+      it '#depth - returns an integer of the number of edges from a node up to the root' do
+        my_tree = Tree.new
+        array = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+        my_tree.build_tree(array)
+        depth = my_tree.depth(90)
+        expect(depth).to eq(2)
+      end
+
+      it '#depth - returns an integer of the number of edges from a node up to the root' do
+        my_tree = Tree.new
+        array = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+        my_tree.build_tree(array)
+        depth = my_tree.depth(50)
+        expect(depth).to eq(0)
+      end
+
+      it '#depth - returns an integer of the number of edges from a node up to the root' do
+        my_tree = Tree.new
+        array = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+        my_tree.build_tree(array)
+        depth = my_tree.depth(100)
+        expect(depth).to eq(3)
+      end
+
+      it '#depth - returns -1 when the data value does not exist in the tree' do
+        my_tree = Tree.new
+        array = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+        my_tree.build_tree(array)
+        depth = my_tree.depth(10000)
+        expect(depth).to eq(-1)
+      end
+    end
+
+    context 'Tree traversal' do
+      before(:each) do
+        @my_tree = Tree.new
+        array = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+        @my_tree.build_tree(array)
+      end
+
+      it '#level_order - returns an array with all items in the tree in level order' do
+        level_order = @my_tree.level_order
+        expect(level_order).to eq([50, 20, 80, 10, 30, 60, 90, 40, 70, 100])
+      end
+
+      it '#level_order_recursive - returns an array with all items in the tree in level order' do
+        level_order = @my_tree.level_order_recursive
+        expect(level_order).to eq([50, 20, 80, 10, 30, 60, 90, 40, 70, 100])
+      end
+
+      it '#inorder - returns an array with all items inorder' do
+        inorder = @my_tree.inorder
+        expect(inorder).to eq([10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+      end
+
+      it '#preorder - returns an array with all items in inorder' do
+        preorder = @my_tree.preorder
+        expect(preorder).to eq([50, 20, 10, 30, 40, 80, 60, 70, 90, 100])
+      end
+
+      it '#postorder - returns an array with all items in postorder' do
+        postorder = @my_tree.postorder
+        expect(postorder).to eq([10, 40, 30, 20, 70, 60, 100, 90, 80, 50])
+      end
+    end
+
+    context 'Checks if the tree is balanced?' do
+      before(:each) do
+        @my_tree = Tree.new
+        array = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+        @my_tree.build_tree(array)
+      end
+
+      it '#balanced? - This tree is balanced' do
+        result = @my_tree.balanced?
+        expect(result).to be true
+      end
+
+      it '#balanced? - This tree is not balanced' do
+        @my_tree.insert(1000)
+        @my_tree.insert(2000)
+        @my_tree.insert(3000)
+        @my_tree.insert(4000)
+        @my_tree.insert(5000)
+        result = @my_tree.balanced?
+        expect(result).to be false
+      end
+
+      it '#balanced? - This tree is not balanced' do
+        @my_tree.insert(1)
+        @my_tree.insert(2)
+        @my_tree.insert(3)
+        @my_tree.insert(4)
+        @my_tree.insert(5)
+        result = @my_tree.balanced?
+        expect(result).to be false
+      end
+    end
+
+    context '#rebalance - Rebalances the tree when there is an unbalanced tree' do
+      before(:each) do
+        @my_tree = Tree.new
+        array = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+        @my_tree.build_tree(array)
+      end
+
+      it '#rebalance - Rebalances an unbalanced tree' do
+        original_tree = @my_tree.balanced?
+        expect(original_tree).to be true
+        @my_tree.insert(1000)
+        @my_tree.insert(2000)
+        @my_tree.insert(3000)
+        @my_tree.insert(4000)
+        @my_tree.insert(5000)
+        unbalanced_tree = @my_tree.balanced?
+        expect(unbalanced_tree).to be false
+        @my_tree.rebalance
+        rebalanced_tree = @my_tree.balanced?
+        expect(rebalanced_tree).to be true
+      end
+
+      it '#rebalance - Rebalances an unbalanced tree' do
+        original_tree = @my_tree.balanced?
+        expect(original_tree).to be true
+        @my_tree.insert(1)
+        @my_tree.insert(2)
+        @my_tree.insert(3)
+        @my_tree.insert(4)
+        @my_tree.insert(5)
+        unbalanced_tree = @my_tree.balanced?
+        expect(unbalanced_tree).to be false
+        @my_tree.rebalance
+        rebalanced_tree = @my_tree.balanced?
+        expect(rebalanced_tree).to be true
+      end
     end
   end
 end
